029e1b40e1ce04fe815f7745a5f36e2542959ace
RottenLog <ivanlogna@gmail.com>
2024-06-24 21:15:53 +0300

    [0.0.2] add all the necessary APIs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,7 +14,7 @@ type Knowledge struct {
 	TimeMarker string `json:"timemarker"`
 }
 
-var knowledges = []Knowlenge{}
+var knowledges = []Knowledge{}
 var idKnowlenge int
 
 func main() {
@@ -22,6 +23,7 @@ func main() {
 	knowledgeR.Path("").Methods(http.MethodGet).HandlerFunc(getAllKnowledges)
 	knowledgeR.Path("").Methods(http.MethodPost).HandlerFunc(createKnowledge)
 	knowledgeR.Path("/{id}").Methods(http.MethodGet).HandlerFunc(getKnowledge)
+	knowledgeR.Path("/{id}").Methods(http.MethodPut).HandlerFunc(changeKnowledge)
 	knowledgeR.Path("/{id}").Methods(http.MethodDelete).HandlerFunc(deleteKnowledge)
 
 	fmt.Println("Greetings! Start listening")
@@ -29,17 +31,100 @@ func main() {
 }
 
 func getAllKnowledges(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Nothing")
+	w.Header().Add("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(knowledges); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding response object", http.StatusInternalServerError)
+	}
 }
 
 func createKnowledge(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Nothing")
+
+	k := Knowledge{}
+
+	if err := json.NewDecoder(r.Body).Decode(&k); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error decoding responce object", http.StatusBadRequest)
+		return
+	}
+
+	knowledges = append(knowledges, k)
+
+	response, err := json.Marshal(&k)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding responce object", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Context-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
+func indexById(knowledges []Knowledge, id string) int {
+	for i := 0; i < len(knowledges); i++ {
+		if knowledges[i].ID == id {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func getKnowledge(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Nothing")
+	id := mux.Vars(r)["id"]
+	index := indexById(knowledges, id)
+
+	if index < 0 {
+		http.Error(w, "Knowledge not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(knowledges[index]); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding responce object", http.StatusInternalServerError)
+		return
+	}
+}
+
+func changeKnowledge(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	index := indexById(knowledges, id)
+
+	if index < 0 {
+		http.Error(w, "Knowledge not found", http.StatusNotFound)
+		return
+	}
+
+	k := Knowledge{}
+	if err := json.NewDecoder(r.Body).Decode(&k); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error decoding responce object", http.StatusBadRequest)
+		return
+	}
+
+	knowledges[index] = k
+
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(&k); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding responce object", http.StatusInternalServerError)
+		return
+	}
 }
 
 func deleteKnowledge(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Nothing")
+	id := mux.Vars(r)["id"]
+	index := indexById(knowledges, id)
+
+	if index < 0 {
+		http.Error(w, "Knowledge not found", http.StatusNotFound)
+		return
+	}
+
+	knowledges = append(knowledges[:index], knowledges[index+1:]...)
+	w.WriteHeader(http.StatusOK)
 }
